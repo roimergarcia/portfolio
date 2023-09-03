9b800aea569a6aa4381f1ef10214d231f98ffa95
Roimer Garcia <roimergarcia@gmail.com>
2023-09-02 21:54:46 -0400

    esLint config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,6 @@ export default defineConfig({
             input: ['resources/css/*.css', 'resources/js/app.js', 'resources/images/**'],
             refresh: true,
         }),
+        eslint({fix: true}),
     ],
 });
