6bd9c5966882a650810c40ce1852e95d1578e961
Roimer Garcia <roimergarcia@gmail.com>
2024-01-06 14:51:00 -0400

    Changed logo and basic color scheme

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Roimer - Portafolio</title>
-        <link rel="icon" type="image/svg" href="{{ Vite::asset('resources/images/logo-vt.svg') }}">
+        <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/images/logo-vt.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
