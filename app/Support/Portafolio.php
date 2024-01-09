9e1a74197a369a110c30ac35e78a2b2fd6795542
Roimer Garcia <roimergarcia@gmail.com>
2024-01-09 19:50:42 -0400

    Pasé textos a constantes y traducciones en Acerca De Mi

--- app/Support/Portafolio.php
+++ app/Support/Portafolio.php
@@ -4,4 +4,6 @@ class Portafolio {
     const CONTACT_EMAIL = 'roimer@variabletecnica.com';
     const PERSONAL_BLOG = 'variabletecnica.com';
     const PERSONAL_BLOG_URL = 'https://variabletecnica.com';
+    const PERSONAL_GITHUB = 'https://github.com/roimergarcia';
+    const PERSONAL_LINKEDIN = 'https://www.linkedin.com/in/roimergarcia';
 }
--- resources/views/components/secciones/acercademi.blade.php
+++ resources/views/components/secciones/acercademi.blade.php
@@ -2,18 +2,22 @@
     <div class="seccion__contenedor">
         <img class="seccion__foto-perfil" src="{{ Vite::asset('resources/images/roimer-garcia.jpg') }}" loading="lazy" alt="Roimer García">
         <div style="display: flex; gap:0.5em; flex-direction: column;">
-            <h2 class="about-me__title">Roimer García</h2>    
+
+            <h2 class="about-me__title">Roimer García</h2>
             <div class="about-me__subtitle">{{__("Senior Web Developer")}}</div>
-            <div class="seccion__about-me__social-container">
-                <a href="https://github.com/roimergarcia"><i class="fa-brands fa-github"></i>Github</a>
-                <a href="https://www.linkedin.com/in/roimergarcia"><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
-                <a href="https://twitter.com/roimergarcia"><i class="fa-brands fa-square-x-twitter"></i>Twitter</a>
-            </div>
+            
             <div>
                 <span>{{__('Email to me at:')}}</span>
                 <a href="mailto:{{Portafolio::CONTACT_EMAIL}}"><i class="fa-clear fa-email"></i>{{Portafolio::CONTACT_EMAIL}}</a>
                 <x-botoncopiar textoACopiar='{{Portafolio::CONTACT_EMAIL}}' />
             </div>
+
+            <div class="seccion__about-me__social-container">
+                <a href="{{Portafolio::PERSONAL_GITHUB}}"><i class="fa-brands fa-github"></i>Github</a>
+                <a href="{{Portafolio::PERSONAL_LINKEDIN}}"><i class="fa-brands fa-linkedin"></i>LinkedIn</a>
+                <a href="{{Portafolio::PERSONAL_BLOG_URL}}"><i class="fa-brands fa-wordpress-simple"></i>{{__('Programming Blog')}}</a>
+            </div>
+
         </div>
     </div>
 </section>
